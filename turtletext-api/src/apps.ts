1c4e10b8d7087fe8fdeb2508d5e719611be984e5
Dragos Rotaru <dragos@rotaru.co>
2019-07-26 17:26:59 -0400

    Made a mistake on purpose

--- turtletext-api/src/apps.ts
+++ turtletext-api/src/apps.ts
@@ -5,11 +5,12 @@ export type App = {
   accentColor: string;
 };
 
-export const apps: App[] = [
-  {
+export const apps: App[] = [];
+/*   {
     id: "8dd52f8a-6061-416b-84b3-ea841a11cd06",
     phone: "+16472951647",
     mainColor: "white",
     accentColor: "#0077ae",
   },
 ];
+ */
