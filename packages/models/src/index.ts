99ec5d052e07ac04d8ad0d1cfb822af0f02a3f7c
Dragos Rotaru <dragos@rotaru.co>
2019-07-26 22:11:30 -0400

    Created Models Package

---
+++ packages/models/src/index.ts
@@ -0,0 +1,7 @@
+export * from "./coordinates";
+export * from "./establishment";
+export * from "./google-autocomplete-result";
+export * from "./lifestyle";
+export * from "./school";
+export * from "./score";
+export * from "./search-result";
