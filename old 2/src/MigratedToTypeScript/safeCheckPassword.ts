4e1cf525a2e4221c7b2c8e1cde00c612c99a323a
Dragos Rotaru <dragos@rotaru.co>
2019-07-31 17:58:57 -0400

    Removed Old 2

--- old 2/src/MigratedToTypeScript/safeCheckPassword.ts
+++
@@ -1,8 +0,0 @@
-export function safeCheckPassword(a: string, b: string): boolean {
-  let mismatch = 0;
-  for (let i = 0; i < a.length; ++i) {
-    mismatch |= a.charCodeAt(i) ^ b.charCodeAt(i);
-  }
-  // returns true if password is correct
-  return mismatch === 0;
-}
