82cdd6c734150e15528eff7c8624262f06537194
Dragos Rotaru <dragos@rotaru.co>
2019-08-11 12:55:03 -0400

    Removed unecessary Endpoint

--- packages/text/src/endpoints.ts
+++ packages/text/src/endpoints.ts
@@ -1,4 +1,3 @@
 import { API_ENDPOINT } from "@huckleberry/core";
 
 export const EVENTS_ENDPOINT = API_ENDPOINT + "/events";
-export const STATIC_ENDPOINT = ""; // "https://static.huckleberry.app/";
