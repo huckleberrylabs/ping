fe9b3eb6839af17d67c72500e03184985be8e808
Foghorn Leghorn <foghorn@network-device.local>
2022-04-19 13:15:08 -0400

    removed TLS from API. It was needed previously for serverless deployment, but now I will use Apache as a reverse proxy

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,34 +1,14 @@
-import fs from "fs";
-import https from "https";
 import http, { Server } from "http";
-import { Env, Logger } from "@huckleberrylabs/ping-core";
+import { Logger } from "@huckleberrylabs/ping-core";
 import { HTTP, WSS } from "./adapters";
-
-const env = Env.Get();
 
 const app = HTTP.C();
 
 let server: Server;
 let port: number;
-if (env === "production") {
-  // TLS Encryption
-  const credentials = {
-    key: fs.readFileSync(
-      "/etc/letsencrypt/live/live.ping.buzz/privkey.pem",
-      "utf8"
-    ),
-    cert: fs.readFileSync(
-      "/etc/letsencrypt/live/live.ping.buzz/fullchain.pem",
-      "utf8"
-    ),
-  };
-  server = https.createServer(credentials, app);
-  port = 443;
-} else {
-  server = http.createServer(app);
-  port = 8000;
-}
 
+server = http.createServer(app);
+port = 8000;
 server.listen(port, () =>
   Logger(HTTP.Name, "info", `server started on ${port}`)
 );
